04ce90f599da2ed2c9e0b38d7cb071dfa04150fc
Yubo Liang <62066004+LeonLiang-9652@users.noreply.github.com>
2022-09-15 23:23:22 +0800

    Update DIN.py

--- ranking/RankingModels/DIN.py
+++ ranking/RankingModels/DIN.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
 
-# Training samples path, change to your local path
 training_samples_file_path = tf.keras.utils.get_file("trainingSamples.csv",
                                                      "/yuboliang/datasets/recsys/movielens/trainSamles.py")
-# Test samples path, change to your local path
+
 test_samples_file_path = tf.keras.utils.get_file("testSamples.csv",
                                                  "/yuboliang/datasets/recsys/movielens/testSamles.py")
 
